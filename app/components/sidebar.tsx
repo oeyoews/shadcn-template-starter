dc55deb485aa80141427650209a5f53a8fc12b5c
oeyoews <jyao4783@gmail.com>
2024-05-12 19:55:14 +0800

    update homepage

---
+++ app/components/sidebar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { LinkMenu } from './menu';
+import { Package2 } from 'lucide-react';
+
+export function Sidebar() {
+  return (
+    <div className="hidden border-r bg-muted/40 md:block">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <Link href="/" className="flex items-center gap-2 font-semibold">
+            <Package2 className="h-6 w-6" />
+            <span className="">HomePage</span>
+          </Link>
+        </div>
+        <div className="flex-1">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <LinkMenu text="Dashboard" route="/" icon="home" />
+            <LinkMenu text="Product" route="/products" icon="shopping-cart" />
+            <LinkMenu text="Customers" route="/customers" icon="users" />
+            <LinkMenu text="Analytics" route="/analytics" icon="line-chart" />
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,9 @@
-import Link from 'next/link';
-import {
-  Bell,
-  CircleUser,
-  Home,
-  LineChart,
-  Menu,
-  Package,
-  Package2,
-  Search,
-  ShoppingCart,
-  Users,
-} from 'lucide-react';
-import { LinkMenu } from './components/menu';
-
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Input } from '@/components/ui/input';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sidebar } from './components/sidebar';
 
 export default function Dashboard() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Package2 className="h-6 w-6" />
-              <span className="">HomePage</span>
-            </Link>
-          </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <LinkMenu text="Dashboard" route="/" icon="home" />
-              <LinkMenu text="Product" route="/products" icon="shopping-cart" />
-              <LinkMenu text="Customers" route="/customers" icon="users" />
-              <LinkMenu text="Analytics" route="/analytics" icon="line-chart" />
-            </nav>
-          </div>
-        </div>
-      </div>
+      <Sidebar />
       <div className="flex flex-col">
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           <div className="flex items-center">
