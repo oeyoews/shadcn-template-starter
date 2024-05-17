26e3cb198173a4191e178cc884c144e2912f5b2f
oeyoews <jyao4783@gmail.com>
2024-05-17 13:55:28 +0800

    fix: update usesearchparams

--- app/song/page.tsx
+++ app/song/page.tsx
@@ -2,10 +2,13 @@
 
 import { useSearchParams } from 'next/navigation';
 import Player from '../components/Player';
+import { Suspense } from 'react';
+
+// https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
 
 export default function SongPage() {
-  const params = useSearchParams();
-  const id = params.get('id');
+  // const params = useSearchParams();
+  // const id = params.get('id');
 
   return <></>;
 }
